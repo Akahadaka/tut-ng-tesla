809710159acdfcdfabc622f3101b9521637cd842
andrew.nash <akahadaka@gmail.com>
2017-02-20 18:01:28 +0200

    New modules and service

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+// angular
+import { BrowserModule }        from '@angular/platform-browser';
+import { NgModule }             from '@angular/core';
+
+// modules
+import {TeslaBatteryModule}     from './tesla-battery/tesla-battery.module';
+
+// app
+import { AppComponent }         from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    TeslaBatteryModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
