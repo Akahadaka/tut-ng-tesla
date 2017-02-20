330db22eaff100fd1e89f6308bc8993b98f0e8b4
andrew.nash <akahadaka@gmail.com>
2017-02-20 17:54:49 +0200

    ng new tesla --skip-install --style=scss

---
+++ src/app/tesla-battery/tesla-battery.module.ts
@@ -0,0 +1,25 @@
+// angular
+import { NgModule }                     from '@angular/core';
+import { CommonModule }                 from '@angular/common';
+import { ReactiveFormsModule }          from '@angular/forms';
+
+// services
+import { TeslaBatteryService }          from './tesla-battery.service';
+
+// app
+import { TeslaBatteryComponent }        from './tesla-battery.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+      TeslaBatteryComponent
+  ],
+  providers: [
+      TeslaBatteryService
+  ],
+  exports: []
+})
+export class TeslaBatteryModule { }
